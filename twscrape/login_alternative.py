9fb97cafe6915516d9f7319c1fdc86dc8854bc77
Darknessest <32790500+darknessest@users.noreply.github.com>
2024-09-20 14:06:31 -0600

    added support for email 2fa

--- twscrape/login_alternative.py
+++ twscrape/login_alternative.py
@@ -1,4 +1,6 @@
+import asyncio
 from copy import deepcopy
+from imaplib import IMAP4_SSL
 
 import tenacity
 from DrissionPage import ChromiumOptions, SessionOptions, WebPage
@@ -7,39 +9,64 @@ from fake_useragent import UserAgent
 from loguru import logger
 
 from .account import Account
-from .gmail import GmailCredentials
-from .imap import imap_login
+from .gmail import GmailCredentials, gmail_get_email_code
+from .imap import imap_get_email_code, imap_login
 from .login import LoginConfig
 
 
 class PageLoadError(Exception):
     pass
 
+
+def login_confirm_email_code(
+    *,
+    manual: bool = False,
+    gmail_credentials: GmailCredentials | None = None,
+    imap=None,
+    username: str = None,
+    email: str = None,
+) -> str:
+    if manual:
+        print(f"Enter email code for {username} / {email}")
+        value = input("Code: ")
+        value = value.strip()
+
+    elif gmail_credentials:
+        logger.trace(f"Getting email code for {ctx.acc.username} through Gmail")
+        value = gmail_get_email_code(gmail_credentials)
+    else:
+
+        now_time = utc.now() - timedelta(seconds=30)
+        value = await imap_get_email_code(ctx.imap, ctx.acc.email, now_time)
+
+    return value
+
+
 # TODO: create a exception handler to kill the page
 @tenacity.retry(stop=tenacity.stop_after_attempt(3), wait=tenacity.wait_fixed(5))
 def login_with_drissionpage(
-        username: str,
-        password: str,
-        email: str,
-        email_password: str,
-        gmail_credentials: GmailCredentials | None = None,
-        base_data_path: str = "drission"
+    username: str,
+    password: str,
+    email: str | None,
+    imap: IMAP4_SSL | None = None,
+    gmail_credentials: GmailCredentials | None = None,
+    base_data_path: str = "drission",
 ) -> tuple[dict, dict] | tuple[None, None]:
     # element constants
     LOGIN_SPAN_TEXT = "Phone, email, or username"
     NEXT_BUTTON_TEXT = "Next"
     EMAIL_OR_PHONE_TEXT = "Phone or email"
-    PASSWORD_TEXT = "Password"
     WHAT_IS_HAPPENING_TEXT = "What is happening?!"
 
     # element selectors
     PASSWORD_SELECTOR = "tag:input@type=password"
+    CODEINPUT_SELECTOR = "tag:input@data-testid=ocfEnterTextTextInput"
     LOGIN_BUTTON_SELECTOR = "tag:button@data-testid=LoginForm_Login_Button"
 
     # urls
     LOGIN_URL = "https://x.com/i/flow/login"
 
-
+    # setup the page
     co = ChromiumOptions()
     co.headless(True)
     co.set_user_data_path(f"{base_data_path}/{username}")
@@ -49,14 +76,12 @@ def login_with_drissionpage(
 
     co.set_user_agent(UserAgent().chrome)
     # co.set_load_mode("eager")
-
     so = SessionOptions()
     page = WebPage(chromium_options=co, session_or_options=so)
 
-
+    # The main part
     page.get(LOGIN_URL)
 
-    # Get "Sign in with Apple" span element
     logger.info("waiting for the page to load")
     try:
         login_elem = page.ele(LOGIN_SPAN_TEXT, timeout=30)
@@ -66,13 +91,30 @@ def login_with_drissionpage(
         logger.error("Failed to load the login page")
         raise PageLoadError("Failed to load the login page")
 
-
     login_elem.input(username)
 
     logger.info("Clicking on the next button")
     page.ele(NEXT_BUTTON_TEXT).click()
 
-    # TODO: add a section to handle the email-based login
+    try:
+        code_elem = page.ele(CODEINPUT_SELECTOR, timeout=5)
+        code_elem.hover()
+        code_elem.click()
+
+        # get the code from email
+        logger.trace(f"Getting email code for {username} through Gmail")
+        if gmail_credentials:
+            code = gmail_get_email_code(gmail_credentials)
+        elif imap and email:
+            code = asyncio.run(imap_get_email_code(imap, email))
+        else:
+            logger.error("No gmail or imap provided. Can't get the code")
+            page.quit()
+            return None, None
+
+        code_elem.input(code)
+    except ElementNotFoundError:
+        logger.debug("No element with 'Confirmation code'")
 
     # check if they are asking for email
     try:
@@ -84,15 +126,25 @@ def login_with_drissionpage(
         logger.trace("They are not asking for email")
 
     # wait for the Password input
-    password_elem = page.ele(PASSWORD_TEXT, timeout=10)
-    password_elem.click()
-    password_elem.input(password)
+    try:
+        password_elem = page.ele(PASSWORD_SELECTOR, timeout=10)
+        password_elem.click()
+        password_elem.input(password)
+    except ElementNotFoundError:
+        logger.error("Password input element not found")
+        page.quit()
+        return None, None
 
     # Find all button elements
     logger.trace("waiting for the login button")
-    login_button = page.ele(LOGIN_BUTTON_SELECTOR, timeout=10)
-    login_button.hover()
-    login_button.click()
+    try:
+        login_button = page.ele(LOGIN_BUTTON_SELECTOR, timeout=10)
+        login_button.hover()
+        login_button.click()
+    except ElementNotFoundError:
+        logger.error("Login button not found")
+        page.quit()
+        return None, None
 
     try:
         # find an element with "Wrong password"
@@ -125,29 +177,34 @@ def login_with_drissionpage(
     return cookies, headers
 
 
-async def login_alternative(acc: Account, cfg: LoginConfig | None = None, base_data_path: str = "drission") -> Account:
+def login_alternative(
+    acc: Account, cfg: LoginConfig | None = None, base_data_path: str = "drission"
+) -> Account:
     log_id = f"{acc.username} - {acc.email}"
     if acc.active:
         logger.info(f"account already active {log_id}")
         return acc
 
     cfg, imap, gmail_creds = cfg or LoginConfig(), None, None
-    if cfg.email_first and not cfg.manual:
-        imap = await imap_login(acc.email, acc.email_password)
-    if cfg.gmail and acc.gmail_credentials:
-        acc.gmail_credentials
+    if cfg.email_first and not cfg.manual and not imap:
+        imap = asyncio.run(imap_login(acc.email, acc.email_password))
+    if cfg.gmail and acc.gmail_credentials and not gmail_creds:
+        gmail_creds = acc.gmail_credentials
 
     # run drissionpage script
     cookies, headers = login_with_drissionpage(
         username=acc.username,
         password=acc.password,
         email=acc.email,
-        email_password=acc.email_password,
-        gmail_credentials=acc.gmail_credentials,
-        base_data_path=base_data_path
+        gmail_credentials=gmail_creds,
+        base_data_path=base_data_path,
     )
 
-    acc.active = True
-    acc.headers = {k: v for k, v in headers.items()}
-    acc.cookies = {k: v for k, v in cookies.items()}
+    if cookies and headers:
+        acc.active = True
+        acc.headers = {k: v for k, v in headers.items()}
+        acc.cookies = {k: v for k, v in cookies.items()}
+    else:
+        acc.active = False
+
     return acc
