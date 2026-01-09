868dea27b57abf8b97b9e3320bcaca09daef6955
darknessest <32790500+darknessest@users.noreply.github.com>
2026-01-09 11:11:41 -0300

    refactor(get_scripts): use a single AsyncClient instance for HTTP requests

--- _get_gql_ops.py
+++ _get_gql_ops.py
@@ -11,33 +11,36 @@ async def get_scripts():
     cache_dir = "/tmp/twscrape-ops"
     os.makedirs(cache_dir, exist_ok=True)
 
-    text = await get_tw_page_text("https://x.com/elonmusk")
-    urls = list(get_scripts_list(text))
+    async with httpx.AsyncClient() as client:
+        text = await get_tw_page_text("https://x.com/elonmusk", clt=client)
+        urls = list(get_scripts_list(text))
 
-    v = text.split("/client-web/main.")[1].split(".")[0]
-    urls.append(script_url("main", v))
+        v = text.split("/client-web/main.")[1].split(".")[0]
+        urls.append(script_url("main", v))
 
-    urls = [
-        x
-        for x in urls
-        if "/i18n/" not in x and "/icons/" not in x and "react-syntax-highlighter" not in x
-    ]
+        urls = [
+            x
+            for x in urls
+            if "/i18n/" not in x
+            and "/icons/" not in x
+            and "react-syntax-highlighter" not in x
+        ]
 
-    scripts = []
-    for i, x in enumerate(urls, 1):
-        cache_path = os.path.join(cache_dir, x.split("/")[-1].split("?")[0])
-        if os.path.exists(cache_path):
-            with open(cache_path) as fp:
-                scripts.append(fp.read())
-            continue
+        scripts = []
+        for i, x in enumerate(urls, 1):
+            cache_path = os.path.join(cache_dir, x.split("/")[-1].split("?")[0])
+            if os.path.exists(cache_path):
+                with open(cache_path) as fp:
+                    scripts.append(fp.read())
+                continue
 
-        print(f"({i:3d} / {len(urls):3d}) {x}")
-        rep = await httpx.AsyncClient().get(x)
-        rep.raise_for_status()
+            print(f"({i:3d} / {len(urls):3d}) {x}")
+            rep = await client.get(x)
+            rep.raise_for_status()
 
-        with open(cache_path, "w") as fp:
-            fp.write(rep.text)
-        scripts.append(rep.text)
+            with open(cache_path, "w") as fp:
+                fp.write(rep.text)
+            scripts.append(rep.text)
 
     return scripts
 
@@ -45,7 +48,11 @@ async def get_scripts():
 async def main():
     with open("./twscrape/api.py") as fp:
         ops = [x.strip() for x in fp.read().split("\n")]
-        ops = [x.split("=")[0].removeprefix("OP_").strip() for x in ops if x.startswith("OP_")]
+        ops = [
+            x.split("=")[0].removeprefix("OP_").strip()
+            for x in ops
+            if x.startswith("OP_")
+        ]
 
     all_pairs = {}
     for txt in await get_scripts():
